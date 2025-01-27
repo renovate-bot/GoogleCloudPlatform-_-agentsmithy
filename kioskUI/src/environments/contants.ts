d696ba5eff2e126b2ae12cc03d275e014afda9b8
Ivan Rufino <ivanerufino@google.com>
2025-01-27 11:36:15 -0300

    feature: Migrating kioskUI code outside soon-to-be open-source repository

--- kioskUI/src/environments/contants.ts
+++
@@ -1,9 +0,0 @@
-export const TEMPLATE_NAMES =  new Map().set('SINGLE_PLAYBOOK', {displayName: 'Conversational App Single Playbook', name: 'conversational-app-single-playbook'})
-                                        .set('MULTIPLE_PLAYBOOK', {displayName: 'Conversational App Multi Playbook', name: 'conversational-app-multi-playbook'})
-                                        .set('AGENT_BUILDER_SEARCH', {displayName: 'Website Search using Agent Builder', name: 'website-search-using-agent-builder'})
-                                        .set('AGENT_BUILDER_DOCUMENT_SEARCH', {displayName: 'Document Search using Agent Builder', name: 'document-search-using-agent-builder'});
-
-export interface template {
-    displayName: string;
-    name: string
-}
